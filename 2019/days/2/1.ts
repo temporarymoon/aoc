f7aa6c195b853dbcd36fd3f0ff392ec37f69d0ab
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-02 18:47:03 +0200

    feat(2): solved day 2

--- 2019/days/2/1.ts
+++ 2019/days/2/1.ts
@@ -1,12 +1,39 @@
-import fs from "fs-extra";
+import fs from "fs";
 
-fs.readFile("./input.txt")
-  .then(r =>
-    r
-      .toString()
-      .split("\n")
-      .map(Number)
-      .map(v => Math.floor(v / 3) - 2)
-      .reduce((a, b) => a + b, 0)
-  )
-  .then(console.log);
+const content = fs.readFileSync("./input.txt");
+const array = content
+  .toString()
+  .split(",")
+  .map(Number);
+
+array[1] = 12;
+array[2] = 2;
+
+for (let index = 0; index < array.length; index += 4) {
+  const element = array[index];
+
+  switch (element) {
+    case 1:
+      const sum = array[array[index + 1]] + array[array[index + 2]];
+      array[array[index + 3]] = sum;
+
+      continue;
+    case 2:
+      const product = array[array[index + 1]] * array[array[index + 2]];
+      array[array[index + 3]] = product;
+
+      console.log("doing a product");
+
+      continue;
+    case 99:
+      // force break out of loop
+      index = array.length;
+      console.log("out", index);
+
+      continue;
+    default:
+      console.log("wrong");
+  }
+}
+
+console.log(array[0]);
