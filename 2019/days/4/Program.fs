ae28669865b178720b82c3ba48d4f3eba0586283
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-04 09:55:17 +0200

    feat(4): solved day 4 part 1

--- 2019/days/4/Program.fs
+++ 2019/days/4/Program.fs
@@ -1,8 +1,34 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+module Day1A = 
+    let rec checkDigits (x: int) (last: int) (foundDuplicates: bool) =
+        if x = 0 then
+            foundDuplicates
+        else
+            let lastDigit = x % 10
+            let hasDuplicates = last = lastDigit || foundDuplicates
+
+            lastDigit <= last && (checkDigits <| x / 10 <| lastDigit <| hasDuplicates)
+
+    let boolToInt (i: bool) = if i  then 1 else 0
+
+    let rec solve (start: int) (_end: int) (count: int) = 
+        if start <> _end then
+            let currentDiff = checkDigits start 10 false |> boolToInt
+            let partialResult = currentDiff + count
+            
+            solve <| start + 1 <| _end <| partialResult
+        else 
+            count
+
+
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let (start, _end) = (int argv.[0],int argv.[1])
+
+    // Part 1
+    let result = Day1A.solve start _end 0
+
+    printfn "%i" result
     0 // return an integer exit code
