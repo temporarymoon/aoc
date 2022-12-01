bd40a6658d2146236d88af1cbdab97f8571b4bea
Matei Adriel <rafaeladriel11@gmail.com>
2022-12-01 20:07:59 +0100

    Few more changes ig

--- 2022/Aoc22/1/Solution.lean
+++ 2022/Aoc22/1/Solution.lean
@@ -3,9 +3,6 @@ import Aoc22.Utils
 
 abbrev ParsedInput := List (List Nat) 
 
-#check (String.splitOn "234" "234" )
-#check List.toArray
-
 def input := include_str "input"
 def parse_input(input: String): ParsedInput := 
   input.splitOn "\n\n"
@@ -17,12 +14,13 @@ def parse_input(input: String): ParsedInput :=
 def totals(input: ParsedInput): List Nat := 
  input.map .sum
 
-def part1(input: String): Option Nat := input 
+def part1(input: String): Nat := input 
    |> parse_input
    |> totals
    |> List.maximum?
+   |> Option.get!
 
-def part2(input: String) := 
+def part2(input: String): Nat := 
    let totals := input 
       |> parse_input
       |> totals
